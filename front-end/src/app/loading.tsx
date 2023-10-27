2ca7fc1c04868c525a1d8571c928448ad08ec063
Anderson Cavalcante <andersonsilvaacaval@gmail.com>
2023-10-26 21:17:38 -0300

    feat tela de loading da aplicação

---
+++ front-end/src/app/loading.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { Box } from "@mui/material"
+import Image from "next/image"
+import dots_loading from "@/assets/animations/dots_loading.gif"
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <Box sx={{
+      display: 'grid',
+      placeItems: 'center',
+      textAlign: 'center'
+  }}>
+      <Image src={"/logo_supermotos_no_bg.svg"} alt={""} width={400} height={380} />
+      <Image src={dots_loading} alt={""} width={60} height={60}/>
+    </Box>
+  )
+}
+
