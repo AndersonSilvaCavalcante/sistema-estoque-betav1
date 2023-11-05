26f27b6ac5f6b9e3d7c84950ebd545ccc10a34d6
Anderson Cavalcante <andersonsilvaacaval@gmail.com>
2023-11-04 21:25:05 -0300

    feat: input de telefone customizado

--- front-end/src/components/CustomInputs/index.tsx
+++ front-end/src/components/CustomInputs/index.tsx
@@ -1,4 +1,5 @@
 import { TextField } from "@mui/material"
+import { MuiTelInput } from "mui-tel-input"
 
 interface IProps {
     value?: string | number
@@ -15,10 +16,9 @@ interface IProps {
     changeFunction: (e: any) => void,
 }
 
-export const CustomTextInput = ({ value, defaultValue, required, label, name, size, fullWidth, error,type, rows, errorMessage, changeFunction }: IProps) => {
+export const CustomTextInput = ({ value, defaultValue, required, label, name, size, fullWidth, error, type, rows, errorMessage, changeFunction }: IProps) => {
     return (
         <TextField
-            id={error ? "outlined-error-helper-text" : "outlined-basic"}
             type={type}
             error={error}
             defaultValue={defaultValue}
@@ -28,9 +28,29 @@ export const CustomTextInput = ({ value, defaultValue, required, label, name, si
             label={label}
             name={name}
             fullWidth={fullWidth}
-            size={size || "small"}
+            size={size ?? "small"}
             variant="outlined"
             rows={rows}
+            helperText={error ? errorMessage ? errorMessage : "Campo obrigatório!" : null}
+        />
+    )
+}
+
+export const CustomTelInput = ({ value, required, label, name, size, fullWidth, error, errorMessage, changeFunction }: IProps) => {
+    return (
+        <MuiTelInput
+            value={value?.toString()}
+            required={required}
+            fullWidth={fullWidth}
+            error={error}
+            defaultCountry="BR"
+            forceCallingCode
+            disableDropdown
+            flagSize="small"
+            label={label}
+            name={name}
+            onChange={(newValue) => changeFunction(newValue)}
+            size={size ?? "small"}
             helperText={error ? errorMessage ? errorMessage : "Campo obrigatório!" : null}
         />
     )
