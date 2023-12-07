21e1a1e6c419ceab11ed492547120f08a5299262
Anderson Cavalcante <andersonsilvaacaval@gmail.com>
2023-12-07 08:38:28 -0300

    feat: relatório do mês incluindo despesas

--- front-end/src/app/dashboard/page.tsx
+++ front-end/src/app/dashboard/page.tsx
@@ -1,8 +1,7 @@
-//TODO ao abrir aplicativo caso tenha produtos com estoque acionar toast.info
 "use client"
 import ContainerCustom from "@/components/Container"
 import PageHeader from "@/components/PageHeader"
-import { Skeleton } from "@mui/material"
+import { Box, Skeleton } from "@mui/material"
 import { NextPage } from "next"
 import React, { useEffect, useState } from "react"
 
@@ -17,7 +16,7 @@ import moment from "moment"
 import "../../assets/css/dashboard.scss"
 
 
-interface IResumeDay {
+interface IResume {
     title: string
     value: number
 }
@@ -31,9 +30,7 @@ interface IRecordStock {
     dateCreated: Date
 }
 
-
-
-const titles: Array<ITitles> = [
+const titlesNoticeProducts: Array<ITitles> = [
     { label: "Nome", value: 'name' },
     { label: "Estoque Mínimo", value: 'qtdMin' },
     { label: "Estoque Atual", value: 'qtdCurrent' },
@@ -48,28 +45,42 @@ const titlesRecordStock: Array<ITitles> = [
     { label: "Data", value: 'dateCreated', date: true },
 ]
 
-
 const Dashboard: NextPage = () => {
-
-    const [resumeDay, setResumeDay] = useState<Array<IResumeDay> | null>(null)
+    const [resumeDay, setResumeDay] = useState<Array<IResume> | null>(null)
+    const [resumeMonth, setResumeMonth] = useState<Array<IResume> | null>(null)
     const [products, setProducts] = useState<Array<IProduct> | null>(null)
     const [recordStock, setRecordStock] = useState<Array<IRecordStock> | null>(null)
 
     const getResumeDay = async () => {
         try {
-            const { data } = await DashboardService.getDashboard()
+            const { data } = await DashboardService.getResumeDay()
             setResumeDay([
                 { title: "Vendas", value: data[0].qtdSales },
                 // { title: "Serviços Concluídos", value: data[0].qtdOrderService },
                 { title: "Faturamento", value: 'R$' + parseFloat(data[0].revenue).toFixed(2) },
                 { title: "Lucro Líquido", value: 'R$' + parseFloat(data[0].profit).toFixed(2) },
                 { title: "Investimento", value: 'R$' + parseFloat(data[0].investment).toFixed(2) },
-                { title: "Despesas", value: 'R$' + parseFloat(data[0].expenses).toFixed(2) },
-                { title: "Caixa", value: 'R$' + parseFloat(data[0].box).toFixed(2) },
+                //{ title: "Despesas", value: 'R$' + parseFloat(data[0].expenses).toFixed(2) },
+                { title: "Em Caixa", value: 'R$' + parseFloat(data[0].cashier).toFixed(2) },
             ])
         } catch { }
     }
+    const getResumeMonth = async () => {
+        try {
+            const { data } = await DashboardService.getResumeMonth()
+            setResumeMonth([
+                { title: "Vendas", value: data[0].qtdSales },
+                // { title: "Serviços Concluídos", value: data[0].qtdOrderService },
+                { title: "Faturamento", value: 'R$' + parseFloat(data[0].revenue).toFixed(2) },
+                { title: "Lucro Líquido", value: 'R$' + parseFloat(data[0].profit).toFixed(2) },
+                { title: "Investimento", value: 'R$' + parseFloat(data[0].investment).toFixed(2) },
+                { title: "Despesas", value: 'R$' + parseFloat(data[0].expenses).toFixed(2) },
+                { title: "Em Caixa", value: 'R$' + parseFloat(data[0].cashier).toFixed(2) }
+            ])
+        } catch (error) {
 
+        }
+    }
     const getNoticeProducts = async () => {
         try {
             const { data } = await DashboardService.getNoticeProducts()
@@ -87,6 +98,7 @@ const Dashboard: NextPage = () => {
 
     useEffect(() => {
         getResumeDay()
+        getResumeMonth()
         getNoticeProducts()
         getRecordStock()
     }, [])
@@ -94,8 +106,8 @@ const Dashboard: NextPage = () => {
     return (
         <React.Fragment>
             <PageHeader title="Painel Administrativo" />
-            <ContainerCustom title={`Resumo do dia ${moment(new Date()).format("MMMM/YYYY")}`}>
-                <div className="resume-day-grid">
+            <ContainerCustom title={`Resumo do dia | ${moment(new Date()).format("DD/MM/YYYY")}`}>
+                <Box className="resume-grid">
                     {resumeDay && resumeDay.map((resume, index: number) => (
                         <CardCustom key={index}>
                             <p>{resume.title}</p>
@@ -109,12 +121,29 @@ const Dashboard: NextPage = () => {
                             <Skeleton animation="wave" />
                         </CardCustom>
                     ))}
-                </div>
+                </Box>
+            </ContainerCustom>
+            <ContainerCustom title={`Resumo do mês | ${moment(new Date()).format("MMMM/YYYY")}`}>
+                <Box className="resume-grid">
+                    {resumeMonth && resumeMonth.map((resume, index: number) => (
+                        <CardCustom key={index}>
+                            <p>{resume.title}</p>
+                            <p>{resume.value}</p>
+                        </CardCustom>
+                    ))}
+
+                    {!resumeMonth && Array.from(new Array(6)).map((e, index: number) => (
+                        <CardCustom key={index}>
+                            <Skeleton animation="wave" />
+                            <Skeleton animation="wave" />
+                        </CardCustom>
+                    ))}
+                </Box>
             </ContainerCustom>
             <ContainerCustom title="Avisos do estoque">
                 <TableCustom
                     data={products}
-                    titles={titles}
+                    titles={titlesNoticeProducts}
                     addStock={true}
                 />
             </ContainerCustom>
